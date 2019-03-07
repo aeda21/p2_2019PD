1ed6e04d47a6b0c3a742a8c75ed247c288a1a6af
ShubyM <shubymishra20@gmail.com>
2019-03-07 09:03:25 -0500

    Update team7.py

--- team7.py
+++ team7.py
@@ -1,68 +1,44 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
+team_name = 'PACK'
+strategy_name = 'Traitor'
+strategy_description = 'Look at their history'
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+
+numOfBME = 0
+numOfCME = 0
+
+numOfCTHEM = 0
+numOfBTHEM = 0
+
+
+'''We look at history to decide whether or not to betray or collude'''
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    global numOfBME, numOfCME, numOfCTHEM, numOfBTHEM
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+  
+    for moves in my_history:
+        if moves == 'b':
+            numOfBME += 1
+            
+        else:
+            numOfCME += 1
+            
+    else:    
+        for moves in their_history:
+            if moves == 'b':
+                numOfBTHEM += 1
+            
+            else:
+                numOfCTHEM += 1
+                
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+    if numOfBME < 10:
+        return 'b'
+        
+    elif len(their_history) > 0:
+        percentCOfThem = (float(numOfCTHEM) /  float((len(their_history)))) * 100
+        
+        if percentCOfThem >= 80:
+            return 'c'
+            
+        else:
+            return 'b'
